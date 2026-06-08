1325515f57f177e93ea743a9f2d32509f06cade3
eth <121030662+thisiseth@users.noreply.github.com>
2026-06-09 03:17:31 +0700

    readme

--- doorbell-chime/src/doorbell_chime/main/audio.h
+++ doorbell-chime/src/doorbell_chime/main/audio.h
@@ -11,7 +11,7 @@ extern "C" {
 typedef enum {
     AUDIO_BACKEND_NONE = 0, //dummy
     AUDIO_BACKEND_DAC = 1, //esp32, esp32-s2
-    AUDIO_BACKEND_PCM_TO_PDM = 2 //esp32, esp32-s3, esp32-p4
+    AUDIO_BACKEND_PCM_TO_PDM = 2 //everywhere except esp32-s2
 } audio_backend_t;
 
 bool audio_init(audio_backend_t backend, gpio_num_t output_pin);
